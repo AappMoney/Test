588198fda812ccba9a7872056df7f7e58b4a0f73
AappMoney <46736553+AappMoney@users.noreply.github.com>
2019-12-12 22:10:13 +0500

    new code

--- addLetter.js
+++ addLetter.js
@@ -4,10 +4,18 @@ function replaceStr(str) {
     }else{
         let strrLetter = str.trim().split("");
         let lastLetter = strrLetter[strrLetter.length - 1].charCodeAt(0);
-        if(lastLetter === 122) return "A";
-        else return String.fromCharCode(lastLetter + 1);
+        let resultarr = [];
+
+        if(lastLetter === 122) {
+            return "A";
+        }
+        else{
+            resultarr.push(String.fromCharCode(lastLetter + 1));
+            return strrLetter.join("") + resultarr;
+        } 
+            
     }
 }
 
 
-console.log(replaceStr("a"));
+console.log(replaceStr("abc"));
